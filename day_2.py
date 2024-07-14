de7a185537ebc5ab67c061cc234fbf1cb7c091c9
Jessica Kiroff <jesskiroff@gmail.com>
2024-07-14 15:52:47 -0400

    initialize day 2

---
+++ day_2.py
@@ -0,0 +1,6 @@
+for i in [2,4,6,8,10]:
+    print("i= ", i)
+for i in range(10): # this way we can control it from start till finish
+    print("i= ", i)
+for i in range(2,10): #will start at 2 and end in 9
+    print("i= ", i)
