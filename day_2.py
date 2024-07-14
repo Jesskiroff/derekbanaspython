471c0c30ab6cb5b37e685d494c18308710f5bf54
Jessica Kiroff <jesskiroff@gmail.com>
2024-07-14 16:07:09 -0400

    odd number for loop

--- day_2.py
+++ day_2.py
@@ -1,6 +1,22 @@
-for i in [2,4,6,8,10]:
+'''for i in [2,4,6,8,10]:
     print("i= ", i)
 for i in range(10): # this way we can control it from start till finish
     print("i= ", i)
 for i in range(2,10): #will start at 2 and end in 9
-    print("i= ", i)
+    print("i= ", i)'''
+
+#if u divide an even by 2
+#i % 2 == 0 
+#if this comes back as zero, we know that this is even
+
+# i = 2
+# print ((i %2) == 0)
+
+
+#use for loop to loop thru 1-21
+for i in range(1, 21):
+
+#use modulus to check that result is not equal to 0
+    if ((i % 2) != 0):
+#print odds
+        print("i = ", i)
