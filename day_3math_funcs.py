1644058d3bd92c68438f5879f97cca72b36e1829
Jessica Kiroff <jesskiroff@gmail.com>
2024-07-18 23:52:23 -0400

    add days 2 and 3

---
+++ day_3math_funcs.py
@@ -0,0 +1,16 @@
+import math
+
+
+print("ceil(4.4)= ",  math.ceil(4.4)) #5
+
+print("floor(4.4)= ", math.floor(4.4)) #4
+
+print("fabs(-4.4)= ", math.fabs(-4.4)) #4.4
+
+#factorial = 1 * 2 * 3 * 4
+print("factorial(4)", math.factorial(4)) #24
+
+print('fmod()= ', math.fmod(5,4)) #1.0
+
+print('trunc(4.2)= ', math.trunc(4.2)) #4
+print("pow(2,2) = ", math.pow(2,2)) #4.0
---
+++ day_3types.py
@@ -0,0 +1,8 @@
+print (type(3))
+print (type(3.14))
+
+print (type("3"))
+print (type('3'))
+
+
+sample_string = "This is a very imp string"
