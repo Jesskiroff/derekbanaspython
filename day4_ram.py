ae86033d6a17bf5f25856c17e44622f1c8a219f8
Jessica Kiroff <jesskiroff@gmail.com>
2024-07-22 16:34:09 -0400

    practice dy 4 challenge again

--- day4_ram.py
+++ day4_ram.py
@@ -1,20 +1,35 @@
+# #ask 4 a string
+# original_string = input("What is the original sentence? ")
+
+# #cinvert the string to uppercase
+# original_string = original_string.upper()
+# #cnvert the string in2 a list
+# '''def Convert(string):
+#     li = list(string.split(" "))
+#     return li
+# print(Convert(original_string))'''
+# list_of_words = original_string.split()
+# #cycle thru the list 
+# print(list_of_words)
+# for word in list_of_words:
+#     #get the 1st letter of th word and eliminate the newline
+#     print(word[0], end="")
+
+# #add a newline
+# print()
+
+
+
+
 #ask 4 a string
-original_string = input("What is the original sentence? ")
-
-#cinvert the string to uppercase
-original_string = original_string.upper()
-#cnvert the string in2 a list
-'''def Convert(string):
-    li = list(string.split(" "))
-    return li
-print(Convert(original_string))'''
-list_of_words = original_string.split()
-#cycle thru the list 
-print(list_of_words)
-for word in list_of_words:
-
+og_str = input("What is the og sentence? ")
+#convert the str in2 uppercase
+og_str = og_str.upper()
+#convert the string into list
+str_into_li = og_str.split()
+#cycle thru the list
+for word in str_into_li:
     #get the 1st letter of th word and eliminate the newline
-    print(word[0], end="")
-
-#add a newline
+    print(word[0], end ='')
+#add a new line
 print()
