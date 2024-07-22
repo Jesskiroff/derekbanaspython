31bc7a5d853e357d67155bb1eb3a84541e5e0f5c
Jessica Kiroff <jesskiroff@gmail.com>
2024-07-21 23:26:14 -0400

    add RAM challenge

--- day4_ram.py
+++ day4_ram.py
@@ -2,12 +2,19 @@
 original_string = input("What is the original sentence? ")
 
 #cinvert the string to uppercase
-upper_string = original_string.upper()
+original_string = original_string.upper()
 #cnvert the string in2 a list
-def Convert(string):
+'''def Convert(string):
     li = list(string.split(" "))
     return li
-print(Convert(upper_string))
+print(Convert(original_string))'''
+list_of_words = original_string.split()
 #cycle thru the list 
+print(list_of_words)
+for word in list_of_words:
+
     #get the 1st letter of th word and eliminate the newline
+    print(word[0], end="")
+
 #add a newline
+print()
