dca776403115966a0788a89eb280353bc52f5c27
Jessica Kiroff <jesskiroff@gmail.com>
2024-07-26 16:44:16 -0400

    add local vs global funcs distinct comparisons

--- day_5_funcs.py
+++ day_5_funcs.py
@@ -1,4 +1,22 @@
 def add_numbers(num1, num2):
     return num1 + num2
 
-print("5 + 4 = ", add_numbers(5,4))
+#print("5 + 4 = ", add_numbers(5,4))
+
+'''LOCAL VARIABLES'''
+
+def assign_name():
+    name = "Albert"
+assign_name()
+
+def change_name(name):
+    name = "Mark"
+name = "Ryan"
+change_name(name)
+print(name)
+
+def name_change(name):
+    return "Mark"
+name = "Allen"
+name = name_change(name)
+print(name)
