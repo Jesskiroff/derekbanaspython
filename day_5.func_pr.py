7247e7b646439fc69db574d281eb64bc51e59157
Jessica Kiroff <jesskiroff@gmail.com>
2024-07-30 13:46:37 -0400

    add func practice problem

---
+++ day_5.func_pr.py
@@ -0,0 +1,7 @@
+#Solve 4 x
+# x+4 = 9
+# x will always be the first val recieved and you'll only deal w a ddition
+#recieve the str and split inconvert the str into ints
+##convert the result into ints
+#convert the result into str and join it to the string "X = "
+#print()
